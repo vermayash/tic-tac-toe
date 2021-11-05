b86544aa53b89ed1f9717a516c5312e2dd5d8039
yashverma <yash.cverma@gmail.com>
2021-11-05 11:59:46 -0500

    added game functionality

--- src/components/Game.js
+++ src/components/Game.js
@@ -1,20 +1,43 @@
 import React, { useState } from "react";
+import { render } from "react-dom";
 import Board from "./Board";
 import calculateWinner from "./logic.js/calculateWinner";
+
+const styles = {
+  width: "200px",
+  margin: "20px auto",
+};
 
 const Game = () => {
   const [board, setBoard] = useState(Array(9).fill(null));
   const [xIsNext, setXIsNext] = useState(true);
-  const [yIsNext, setYIsNext] = useState(false);
   const winner = calculateWinner(board);
 
-  const onClick = () => {};
+  const onClick = (i) => {
+    const boardCopy = [...board];
+
+    if (winner || boardCopy[i]) return;
+
+    boardCopy[i] = xIsNext ? "X" : "O";
+    setBoard(boardCopy);
+    setXIsNext(!xIsNext);
+  };
   const jumpTo = () => {};
-  const renderMoves = () => {};
+  const renderMoves = () => (
+    <button onClick={() => setBoard(Array(9).fill(null))}>Start Game</button>
+  );
 
   return (
     <div>
       <Board onClick={onClick} squares={board} />
+      <div style={styles}>
+        <p>
+          {winner
+            ? "Winner: " + winner
+            : "Next Player: " + (xIsNext ? "X" : "O")}
+        </p>
+        {renderMoves()}
+      </div>
     </div>
   );
 };
